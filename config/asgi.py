9f781ddc9a38b62ca731cb5d032c20ebf3b8372f
Lance Samonte <samonte.lance.22@gmail.com>
2025-02-07 22:29:03 +0800

    update the asgi settings to include app path and fix env

--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(BASE_DIR / "vite"))
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 
 application = get_asgi_application()
