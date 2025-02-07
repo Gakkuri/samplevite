2bd2fa09dd48a3775c9427402d067266d8c1f6ff
Lance Samonte <samonte.lance.22@gmail.com>
2025-02-07 23:54:13 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@
 # Exit on error
 set -o errexit
 
-RUN echo "installing dependencies"
+echo "installing dependencies"
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements/production.txt
 
-RUN echo "building frontend"
+echo "building frontend"
 # build Vite frontend
 cd frontend
 npm install
 npm run build
 cd ..
 
-RUN echo "collecting static files"
+echo "collecting static files"
 
 # Convert static asset files
 python manage.py collectstatic --no-input --clear
 
-RUN echo "applying migrations"
+echo "applying migrations"
 
 # Apply any outstanding database migrations
 python manage.py migrate
